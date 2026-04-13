4ae8aa00015cae6a9b98eabfb374f44b5672c9bd
maksim.trojan <maksimastrojan@gmail.com>
2026-04-13 11:03:09 +0300

    split to describes

--- tests/Todo.spec.js
+++ tests/Todo.spec.js
@@ -3,142 +3,153 @@ import { test } from '@playwright/test';
 import { TodoPage } from '../pages/TodoPage';
 import { FilterState } from '../pages/helpers/FilterState';
 
-test('add multiple todos', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
+test.describe('Todo Application Tests', () => {
+  test.describe('Adding', () => {
+    test('add multiple todos', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
 
-  await todoPage.addTodos("A", "B", "C");
-  await todoPage.todosShouldBe(['A', 'B', 'C']);
-});
+      await todoPage.addTodos("A", "B", "C");
+      await todoPage.todosShouldBe(['A', 'B', 'C']);
+    });
+  });
 
-test('toggle', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
+  test.describe('Toggling', () => {
+    test('toggle', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
 
-  await todoPage.addTodos("A", "B", "C");
-  await todoPage.toggle('B')
-  await todoPage.activeTodosShouldBe(['A', 'C']);
-  await todoPage.completeTodosShouldBe(['B']);
+      await todoPage.addTodos("A", "B", "C");
+      await todoPage.toggle('B');
+      await todoPage.activeTodosShouldBe(['A', 'C']);
+      await todoPage.completeTodosShouldBe(['B']);
 
-  await todoPage.toggle('B')
-  await todoPage.activeTodosShouldBe(['A', 'B', 'C']);
-  await todoPage.completeTodosShouldBe([]);
-});
+      await todoPage.toggle('B');
+      await todoPage.activeTodosShouldBe(['A', 'B', 'C']);
+      await todoPage.completeTodosShouldBe([]);
+    });
 
-test('toggle all', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
+    test('toggle all', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
 
-  await todoPage.toggleAll();
-  await todoPage.activeTodosShouldBe([]);
-  await todoPage.completeTodosShouldBe(['A', 'B', 'C']);
+      await todoPage.toggleAll();
+      await todoPage.activeTodosShouldBe([]);
+      await todoPage.completeTodosShouldBe(['A', 'B', 'C']);
 
-  await todoPage.toggleAll();
-  await todoPage.activeTodosShouldBe(['A', 'B', 'C']);
-  await todoPage.completeTodosShouldBe([]);
-});
+      await todoPage.toggleAll();
+      await todoPage.activeTodosShouldBe(['A', 'B', 'C']);
+      await todoPage.completeTodosShouldBe([]);
+    });
+  });
 
-test('clear completed', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
+  test.describe('Clearing and Deleting', () => {
+    test('clear completed', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
 
-  await todoPage.addTodos("A", "B", "C");
-  await todoPage.toggle('B');
-  await todoPage.clearCompleted();
-  await todoPage.todosShouldBe(['A', 'C']);
-});
+      await todoPage.addTodos("A", "B", "C");
+      await todoPage.toggle('B');
+      await todoPage.clearCompleted();
+      await todoPage.todosShouldBe(['A', 'C']);
+    });
 
-test('delete a todo', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
+    test('delete a todo', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
 
-  await todoPage.addTodos("A", "B", "C");
-  await todoPage.delete('B');
-  await todoPage.todosShouldBe(['A', 'C']);
-});
+      await todoPage.addTodos("A", "B", "C");
+      await todoPage.delete('B');
+      await todoPage.todosShouldBe(['A', 'C']);
+    });
 
+    test('delete by editing to empty', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
 
-test('delete by editing to empty', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
+      await todoPage.edit('B', '');
 
-  await todoPage.edit('B', '');
+      await todoPage.todosShouldBe(['A', 'C']);
+    });
+  });
 
-  await todoPage.todosShouldBe(['A', 'C']);
-});
+  test.describe('Filtering', () => {
+    test('filters', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
+      await todoPage.toggle('B');
 
+      await todoPage.filter(FilterState.ACTIVE);
+      await todoPage.activeTodosShouldBe(['A', 'C']);
 
-test('filters', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
-  await todoPage.toggle('B');
+      await todoPage.filter(FilterState.COMPLETED);
+      await todoPage.completeTodosShouldBe(['B']);
 
-  await todoPage.filter(FilterState.ACTIVE);
-  await todoPage.activeTodosShouldBe(['A', 'C']);
+      await todoPage.filter(FilterState.ALL);
+      await todoPage.todosShouldBe(['A', 'B', 'C']);
+    });
+  });
 
-  await todoPage.filter(FilterState.COMPLETED);
-  await todoPage.completeTodosShouldBe(['B']);
+  test.describe('Editing', () => {
+    test('edit by enter', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
 
-  await todoPage.filter(FilterState.ALL);
-  await todoPage.todosShouldBe(['A', 'B', 'C']);
-});
+      await todoPage.edit('B', 'B edited');
+      await todoPage.todosShouldBe(['A', 'B edited', 'C']);
+    });
 
-test('edit by enter', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
+    test('edit by focus change', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
 
-  await todoPage.edit('B', 'B edited');
-  await todoPage.todosShouldBe(['A', 'B edited', 'C']);
-});
+      await todoPage.editByTab('B', 'B edited');
+      await todoPage.todosShouldBe(['A', 'B edited', 'C']);
+    });
 
-test('edit by focus change', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
+    test('Start edit and refresh should NOT keep changes', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
 
-  await todoPage.editByTab('B', 'B edited');
-  await todoPage.todosShouldBe(['A', 'B edited', 'C']);
-});
+      await todoPage.startEditing('B', "B edited");
+      await page.reload();
+      await todoPage.todosShouldBe(['A', 'B', 'C']);
+    });
 
-test('Start edit and refresh should NOT keep changes', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
+    test('edit by escape should cancel edit', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A", "B", "C");
 
-  await todoPage.startEditing('B', "B edited");
-  await page.reload();
-  await todoPage.todosShouldBe(['A', 'B', 'C']);
-}
-);
+      await todoPage.editByEscape('B', 'B edited');
+      await todoPage.todosShouldBe(['A', 'B', 'C']);
+    });
+  });
 
-test('edit by escape should cancel edit', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A", "B", "C");
+  test.describe('UI Behavior', () => {
+    test('items left should update correctly', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
+      await todoPage.addTodos("A");
 
-  await todoPage.editByEscape('B', 'B edited');
-  await todoPage.todosShouldBe(['A', 'B', 'C']);
-});
+      await todoPage.itemsLeftShouldBe('1 item left');
 
-test('items left should update correctly', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-  await todoPage.addTodos("A");
+      await todoPage.toggle('A');
+      await todoPage.itemsLeftShouldBe('0 items left');
+    });
 
-  await todoPage.itemsLeftShouldBe('1 item left');
+    test('main and footer should be hidden on fresh open', async ({ page }) => {
+      const todoPage = new TodoPage(page);
+      await todoPage.open();
 
-  await todoPage.toggle('A');
-  await todoPage.itemsLeftShouldBe('0 items left');
-});
-
-test('main and footer should be hidden on fresh open', async ({ page }) => {
-  const todoPage = new TodoPage(page);
-  await todoPage.open();
-
-  await todoPage.todoListShouldBeHidden();
-  await todoPage.footerShouldBeHidden();
+      await todoPage.todoListShouldBeHidden();
+      await todoPage.footerShouldBeHidden();
+    });
+  });
 });
